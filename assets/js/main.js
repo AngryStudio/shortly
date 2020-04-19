f677aa07fc97ce56abb02bd33d336e63df7e334e
Angry Studio <bigdaddy@angrystudio.com>
2020-04-19 12:30:40 +0530

    Preloader & Webkit appearance
    Webkit appearance and preloader fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,10 +41,9 @@
   function display(days, hours, minutes, seconds) {}
 }
 
-
-
-/*-- preloader --*/
-var p = document.createElement("div");
-p.innerHTML = "<div style='z-index:100000' id='owlreporter-preloader'><div id='loader'></div><div class='loader-section section-left'></div><div class='loader-section section-right'></div></div>";
-document.body.insertBefore(p, document.body.firstChild);
-function pageload(){var e=(new Date).getTime(),t=(e-before)/1e3,n=document.getElementById("loadingtime");n.innerHTML="Page load: "+t+" seconds."}window.onload=function(){pageload()},setTimeout(function(){document.body.className+=" loaded"},1500),document.addEventListener?document.addEventListener("DOMContentLoaded",function(){document.removeEventListener("DOMContentLoaded",arguments.callee,!1),domReady()},!1):document.attachEvent&&document.attachEvent("onreadystatechange",function(){"complete"===document.readyState&&(document.detachEvent("onreadystatechange",arguments.callee),domReady())});
+window.onload = function() {
+    var preloader = document.getElementsByClassName('preloader')[0];
+    setTimeout(function(){
+        preloader.style.display = 'none';
+    }, 2000);
+};
